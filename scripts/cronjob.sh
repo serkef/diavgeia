2db933e1265b7a498dc36a105a07e0cad3610247
Sergio Kef <sergiokef@gmail.com>
2020-03-25 19:43:40 +0100

    Remove existing container on cron

--- scripts/cronjob.sh
+++ scripts/cronjob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BUILD_TAG=$(git rev-parse HEAD)
-CRON_CMD='1 0 * * *	docker run --name diavgeia-daily --env-file='$HOME'/diavgeia/diavgeia/.env --restart on-failure diavgeia-daily:'${BUILD_TAG}' --date $(date -d "yesterday" -I)'
+CRON_CMD='1 0 * * *	docker rm diavgeia-daily; docker run --name diavgeia-daily --env-file='$HOME'/diavgeia/diavgeia/.env --restart on-failure diavgeia-daily:'${BUILD_TAG}' --date $(date -d "yesterday" -I)'
 
 
 echo
