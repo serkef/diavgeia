9ef7dc5a0162d354f23656ecba877770ed465976
Sergio Kef <sergiokef@gmail.com>
2020-07-17 00:02:09 +0200

    Add logs

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-exec diavgeia-daily "$@"
+echo "Running diavgeia fetch with args \`$@\`"
+diavgeia-daily "$@"
+
+echo "Uploading data to bucket"
 rclone move $EXPORT_DIR $RCLONE_B2_REMOTE:$BUCKET_NAME/raw --config /home/app/rclone.conf --log-file=$LOG_DIR/rclone_$(date -u +"%Y-%m-%dT%H:%M:%SZ")
