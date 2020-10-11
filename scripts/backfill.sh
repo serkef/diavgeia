2c24d78d5149069470008da0d4946b08fbc2f7d9
Sergio Kef <sergiokef@gmail.com>
2020-10-11 18:24:46 +0200

    Fix backfill start date

--- scripts/backfill.sh
+++ scripts/backfill.sh
@@ -5,7 +5,6 @@ BUILD_TAG=$(git rev-parse HEAD)
 currDay=2000-01-01
 while [ "$currDay" != $(date -I) ]; do
   echo "Running for $currDay"
-  currDay=$(date -I -d "$currDay + 1 day")
   docker run --rm \
   --name diavgeia-daily \
   --env-file=$HOME/diavgeia/diavgeia/.env \
@@ -13,4 +12,5 @@ while [ "$currDay" != $(date -I) ]; do
   --volume=$HOME/.config/rclone/rclone.conf:/home/app/rclone.conf \
   diavgeia-daily:${BUILD_TAG} \
   --date $currDay
+  currDay=$(date -I -d "$currDay + 1 day")
 done
