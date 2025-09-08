76b8deacb6ae5115513c345f07cb71bd88d9218f
Sergio Kef <sergiokef@gmail.com>
2025-09-08 16:01:35 +0200

    feat: add scheduler

--- src/main.py
+++ src/main.py
@@ -8,25 +8,37 @@ from workers.scheduler import Scheduler
 @click.command()
 @click.option(
     "--date-id",
-    required=True,
     type=click.DateTime(formats=["%Y-%m-%d"]),
     help="Date to fetch documents for. Format: YYYY-MM-DD",
 )
-@click.option("--log-path", type=click.Path(), help="Override log path")
-@click.option("--export-path", type=click.Path(), help="Override export path")
-@click.option("--download-pdf", type=bool, help="Override download_pdf setting")
-@click.option("--download-workers", type=int, help="Override download_workers setting")
+@click.option("--log-path", type=click.Path(), help="The path for the logs")
+@click.option("--export-path", type=click.Path(), help="The path for all downloads")
+@click.option(
+    "--download-pdf", default=False, type=bool, help="If set to True, download PDFs"
+)
+@click.option("--download-workers", type=int, help="Number of download workers")
+@click.option("--daemon-mode", type=bool, help="Run in daemon mode")
+@click.option("--schedule-time", type=str, help="Time to run daily job, eg '04:00')")
 @click.option(
     "--log-level",
     type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
-    help="Override log_level setting",
+    help="Log level",
 )
 def main(**cli_args):
     """Main function"""
 
-    Dispatcher(
-        DiavgeiaConfig(**{k: v for k, v in cli_args.items() if v is not None})
-    ).execute()
+    cli_args = {k: v for k, v in cli_args.items() if v is not None}
+    config = DiavgeiaConfig(**cli_args)
+
+    if config.date_id is None and not config.daemon_mode:
+        raise ValueError(
+            "Either DATE_ID must be provided (--date-id) or DAEMON_MODE (--daemon-mode) must be set"
+        )
+
+    if config.date_id is not None:
+        Dispatcher(config).execute()
+    elif config.daemon_mode:
+        Scheduler(config).start_daemon()
 
 
 if __name__ == "__main__":
